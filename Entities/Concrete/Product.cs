f6ac9eb02739b30f95453c27b77d6ced92a02ca1
Melisa Mert <melisamert024@gmail.com>
2020-07-15 19:47:23 +0300

    Projects added in solution and entities class added.

---
+++ Entities/Concrete/Product.cs
@@ -0,0 +1,24 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Concrete
+{
+    public class Product : IEntity
+    {
+        public int Id { get; set; }
+
+        public int PartnerId { get; set; }
+
+        public int CategoryId { get; set; }
+
+        public string Details { get; set; }
+
+        public string Image { get; set; }
+
+        public  decimal Price { get; set; }
+
+        public decimal DiscountPrice { get; set; }
+    }
+}
